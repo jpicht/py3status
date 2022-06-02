28f772a599c23f153f28a123655128f5f8ea0d96
Julian Picht <julian.picht@gmail.com>
2022-06-02 12:36:01 +0200

    modules/external_script implement i3bar protocol

--- py3status/modules/external_script.py
+++ py3status/modules/external_script.py
@@ -1,11 +1,15 @@
 """
 Display output of a given shell command.
 
-Display output of any executable script set by `script_path`. Only the first
-two lines of output will be used. The first line is used as the displayed
-text. If the output has two or more lines, the second line is set as the text
-color (and should hence be a valid hex color code such as #FF0000 for red).
-The script should not have any parameters, but it could work.
+Display output of the shell command set by `script_path`. By default
+(`input_format` = 'text') only the first two lines of output will be used. The
+first line is used as the displayed text. If the output has two or more lines,
+the second line is set as the text color (and should hence be a valid hex color
+code such as #FF0000 for red).
+
+If `input_format` is set to 'i3bar' the input is parsed as a single 'i3bar'
+protocol item. In this mode `format`, `localize`, `strip_output`,
+`convert_numbers`, and `button_show_notification` are ignored.
 
 Configuration parameters:
     button_show_notification: button to show notification with full output
@@ -15,6 +19,8 @@ Configuration parameters:
     convert_numbers: convert decimal numbers to a numeric type
         (default True)
     format: see placeholders below (default '{output}')
+    input_format: specify input format, either 'text' or 'i3bar'
+        (default 'text')
     localize: should script output be localized (if available)
         (default True)
     script_path: script you want to show output of (compulsory)
@@ -32,6 +38,11 @@ external_script {
     format = "my name is {output}"
     script_path = "/usr/bin/whoami"
 }
+
+external_script {
+    script_path = "/home/user/go/bin/k8s_status_i3bar"
+    input_format = "i3bar"
+}
 ```
 
 @author frimdo ztracenastopa@centrum.cz
@@ -43,10 +54,19 @@ example
 {'full_text': 'It is now: Wed Feb 22 22:24:13'}
 """
 
+import json
+from os import chmod, unlink
 import re
 
-STRING_ERROR = "missing script_path"
+STRING_ERROR_MISSING_PATH = "missing script_path"
+STRING_ERROR_INPUT_FORMAT = "invalid input format"
 
+STRING_TEST_SCRIPT = """
+#!/bin/bash
+
+echo '{testdata}'
+
+"""
 
 class Py3status:
     """
@@ -57,18 +77,19 @@ class Py3status:
     cache_timeout = 15
     convert_numbers = True
     format = "{output}"
+    input_format = "text"
     localize = True
     script_path = None
     strip_output = False
 
     def post_config_hook(self):
         if not self.script_path:
-            raise Exception(STRING_ERROR)
+            raise Exception(STRING_ERROR_MISSING_PATH)
+
+        if self.input_format not in ("text", "i3bar"):
+            raise Exception(STRING_ERROR_INPUT_FORMAT)
 
     def external_script(self):
-        output_lines = None
-        response = {}
-        response["cached_until"] = self.py3.time_in(self.cache_timeout)
         try:
             self.output = self.py3.command_output(
                 self.script_path, shell=True, localized=self.localize
@@ -78,6 +99,11 @@ class Py3status:
             output = e.output or e.error
             self.py3.error(output)
 
+        if self.input_format == "i3bar":
+            return json.loads(self.output)
+
+        response = {}
+        response["cached_until"] = self.py3.time_in(self.cache_timeout)
         output_lines = self.output.splitlines()
         if len(output_lines) > 1:
             output_color = output_lines[1]
